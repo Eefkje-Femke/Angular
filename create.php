59a22e0e732fa94ee9e6b8017b1ece823a77ce21
Eefkje-Femke <40299927+Eefkje-Femke@users.noreply.github.com>
2019-07-03 11:54:36 +0200

    create-record-files
    files for adding a record.

---
+++ create.php
@@ -0,0 +1,12 @@
+<form class="" action="create-query.php" method="post">
+  <p>Voornaam: <input type="text" name="vname"></input></p>
+  <p>Achternaam: <input type="text" name="aname"></input></p>
+  <p>Straat: <input type="text" name="straat"></input></p>
+  <p>Huisnummer: <input type="text" name="hnummer"></input></p>
+  <p>Postcode: <input type="text" name="postcode"></input></p>
+  <p>Woonplaats: <input type="text" name="wplaats"></input></p>
+  <p>Telefoonnummer: <input type="text" name="tnum"></input></p>
+  <input type="submit" name="edit" value="edit"></input>
+</form>
+
+<!--bootstrap/Javascript website???-->
